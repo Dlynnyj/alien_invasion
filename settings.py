70686daacbde7dbde7bea3fee59165725115f60a
Kovtan Roman <r.kovtan@gmail.com>
2023-01-14 21:51:36 +0200

    settings reformation))

--- settings.py
+++ settings.py
@@ -1,20 +1,17 @@
 class Settings:
 
     def __init__(self):
-
         """
         screen settings
         """
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-
         """
         ship settings
         """
         self.ship_speed = 1.5
         self.ship_limit = 3
-
         """
         bullets settings
         """
@@ -23,7 +20,6 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
-
         """
         aliens settings
         """
