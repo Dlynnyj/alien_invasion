d7e44afc0aa6ee986f6171b39b18332025eb7c69
Kovtan Roman <r.kovtan@gmail.com>
2023-01-14 18:48:35 +0200

    creating fleet of alien movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,6 +34,7 @@ class Alieninvasion:
             self._check_events()
             self.ship.update()
             self._bullets_update()
+            self._update_aliens()
             self._update_screen()
 
     def _create_fleet(self):
@@ -49,6 +50,21 @@ class Alieninvasion:
         for row_number in range(number_rows):
             for alien_number in range(number_aliens_x):
                 self._create_alien(alien_number, row_number)
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+    def _update_aliens(self):
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _create_alien(self, alien_number, row_number):
         alien = Alien(self)
--- settings.py
+++ settings.py
@@ -22,3 +22,11 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        """
+        aliens settings
+        """
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+
